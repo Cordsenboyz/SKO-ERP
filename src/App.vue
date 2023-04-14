f9dd4aedaa09180b4a518e6507e6a5d872f63ebf
Cordsenboyz <46309019+Cordsenboyz@users.noreply.github.com>
2023-04-14 11:46:32 +0200

    Login system intergration

--- src/App.vue
+++ src/App.vue
@@ -36,15 +36,47 @@ import axios from "axios";
     created: async function(){
       if(localStorage.getItem("token")){
         this.IsLoggingIn = true;
-        var token = localStorage.getItem("token")
-        let userobject = await axios.get(`https://localhost:7203/api/User/getUser`, 
+        let token = localStorage.getItem("token")
+        let expires = localStorage.getItem("expires")
+        if(new Date(expires).getTime() > Date.now()){
+          await axios.get(`https://localhost:7203/api/User/getUser`, 
           {
               headers: { Authorization: `Bearer ${token}` }
+          }).then(response => {
+            store.IsAuthenticated = true
+            role.value = response.data.role;
+            user.data.fullName = response.data.fullName
+            this.IsLoggingIn = false;
           })
-          store.IsAuthenticated = true
-          role.value = userobject.data.role;
-          user.data.username = userobject.data.userName
-          this.IsLoggingIn = false;
+        }
+        else{
+          if(localStorage.getItem("refreshToken")){
+            let refreshToken = localStorage.getItem("refreshToken")
+            let token = localStorage.getItem("token")
+            
+            await axios.post(`https://localhost:7203/api/User/refreshToken`, {
+              token: token,
+              refreshToken: refreshToken
+            }).then(async response => {
+              localStorage.setItem("token", response.data.token.token)
+              localStorage.setItem("refreshToken", response.data.token.refreshToken)
+              localStorage.setItem("expires", response.data.token.expiration)
+
+              await axios.get(`https://localhost:7203/api/User/getUser`, 
+                {
+                    headers: { Authorization: `Bearer ${response.data.token.token}` }
+                }).then(userResponse => {
+                  store.IsAuthenticated = true
+                  role.value = userResponse.data.role;
+                  user.data.fullName = userResponse.data.fullName
+                  this.IsLoggingIn = false;
+                })
+              })
+          } else{
+            this.IsLoggingIn = false;
+            localStorage.clear()
+          }
+        }
       }
     },
   }
